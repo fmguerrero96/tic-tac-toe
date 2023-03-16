6a1d115339eb7bb2c2f54723f7d9b605032696ae
Misael Guerrero <mguerrerowest@gmail.com>
2023-03-15 21:15:26 -0400

    create board array and fuction to displaye game board dynamically

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const Player = (name, symbol) => {
   
   const playerX = Player('PlayerX', 'X');
   const playerO = Player('PlayerO', 'O');
+
+  const gameboard = (() => {
+    const boardContainer = document.querySelector('.board-container');
+    const board = ['','','','','','','','','']
+    let cellIndex = 0 
+    function displayBoard() {
+      board.forEach(function (item){
+        let cell = document.createElement('div');
+        cell.className = ('cell');
+        cell.id = cellIndex
+        boardContainer.appendChild(cell);
+        cellIndex += 1
+      })
+    };
+    
+    return{displayBoard, board};
+  })();
