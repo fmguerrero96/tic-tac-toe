313c85ef2463f11ff6542664aafbf0d9a24f6314
Misael Guerrero <mguerrerowest@gmail.com>
2023-03-15 21:29:54 -0400

    create functions to dosplay tie or win messages

--- main.js
+++ main.js
@@ -40,31 +40,42 @@ const Player = (name, symbol) => {
         })
       }
 
-      function switchPlayer(){
-        if(playerTurn === playerX.symbol){
-          playerTurn = playerO.symbol;
-          message.textContent = `Player ${playerTurn}'s Turn`
-        } else{playerTurn = playerX.symbol
-          message.textContent = `Player ${playerTurn}'s Turn`}
+    function switchPlayer(){
+    if(playerTurn === playerX.symbol){
+        playerTurn = playerO.symbol;
+        message.textContent = `Player ${playerTurn}'s Turn`
+    } else{playerTurn = playerX.symbol
+        message.textContent = `Player ${playerTurn}'s Turn`}
+    }
+
+    function checkWin(){
+    if (gameboard.board[0] === gameboard.board[1] && gameboard.board[0] === gameboard.board[2] && gameboard.board[0] != ''||
+    gameboard.board[3] === gameboard.board[4] && gameboard.board[3] === gameboard.board[5] && gameboard.board[3] != '' ||
+    gameboard.board[6] === gameboard.board[7] && gameboard.board[6] === gameboard.board[8] && gameboard.board[6] != '' ||
+    gameboard.board[0] === gameboard.board[3] && gameboard.board[0] === gameboard.board[6] && gameboard.board[0] != '' ||
+    gameboard.board[1] === gameboard.board[4] && gameboard.board[1] === gameboard.board[7] && gameboard.board[1] != '' ||
+    gameboard.board[2] === gameboard.board[5] && gameboard.board[2] === gameboard.board[8] && gameboard.board[2] != '' ||
+    gameboard.board[0] === gameboard.board[4] && gameboard.board[0] === gameboard.board[8] && gameboard.board[0] != '' ||
+    gameboard.board[2] === gameboard.board[4] && gameboard.board[2] === gameboard.board[6] && gameboard.board[2] != '' )
+            {
+        gameWin()
+        }
+    }
+
+    function checkTie() {
+    if (gameboard.board.every(tieEvent)){
+        gameTie()
+        }
+    }
+
+    const gameWin = () => {
+        if (playerTurn === playerX.symbol){
+          message.textContent = 'Player O Wins'
+        } else {message.textContent = 'Player X Wins'}
+      }
+    
+      const gameTie = () => {
+        message.textContent = "It's a tie"
       }
 
-      function checkWin(){
-        if (gameboard.board[0] === gameboard.board[1] && gameboard.board[0] === gameboard.board[2] && gameboard.board[0] != ''||
-        gameboard.board[3] === gameboard.board[4] && gameboard.board[3] === gameboard.board[5] && gameboard.board[3] != '' ||
-        gameboard.board[6] === gameboard.board[7] && gameboard.board[6] === gameboard.board[8] && gameboard.board[6] != '' ||
-        gameboard.board[0] === gameboard.board[3] && gameboard.board[0] === gameboard.board[6] && gameboard.board[0] != '' ||
-        gameboard.board[1] === gameboard.board[4] && gameboard.board[1] === gameboard.board[7] && gameboard.board[1] != '' ||
-        gameboard.board[2] === gameboard.board[5] && gameboard.board[2] === gameboard.board[8] && gameboard.board[2] != '' ||
-        gameboard.board[0] === gameboard.board[4] && gameboard.board[0] === gameboard.board[8] && gameboard.board[0] != '' ||
-        gameboard.board[2] === gameboard.board[4] && gameboard.board[2] === gameboard.board[6] && gameboard.board[2] != '' )
-             {
-            gameWin()
-          }
-        }
-    
-      function checkTie() {
-        if (gameboard.board.every(tieEvent)){
-          gameTie()
-        }
-      }
   })
