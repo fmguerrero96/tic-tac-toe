fdaf1aba56a3eb7301633b172bff4fe24cf9c1ec
Misael Guerrero <mguerrerowest@gmail.com>
2023-03-15 21:24:47 -0400

    create functions that check for a tie or win

--- main.js
+++ main.js
@@ -47,4 +47,24 @@ const Player = (name, symbol) => {
         } else{playerTurn = playerX.symbol
           message.textContent = `Player ${playerTurn}'s Turn`}
       }
+
+      function checkWin(){
+        if (gameboard.board[0] === gameboard.board[1] && gameboard.board[0] === gameboard.board[2] && gameboard.board[0] != ''||
+        gameboard.board[3] === gameboard.board[4] && gameboard.board[3] === gameboard.board[5] && gameboard.board[3] != '' ||
+        gameboard.board[6] === gameboard.board[7] && gameboard.board[6] === gameboard.board[8] && gameboard.board[6] != '' ||
+        gameboard.board[0] === gameboard.board[3] && gameboard.board[0] === gameboard.board[6] && gameboard.board[0] != '' ||
+        gameboard.board[1] === gameboard.board[4] && gameboard.board[1] === gameboard.board[7] && gameboard.board[1] != '' ||
+        gameboard.board[2] === gameboard.board[5] && gameboard.board[2] === gameboard.board[8] && gameboard.board[2] != '' ||
+        gameboard.board[0] === gameboard.board[4] && gameboard.board[0] === gameboard.board[8] && gameboard.board[0] != '' ||
+        gameboard.board[2] === gameboard.board[4] && gameboard.board[2] === gameboard.board[6] && gameboard.board[2] != '' )
+             {
+            gameWin()
+          }
+        }
+    
+      function checkTie() {
+        if (gameboard.board.every(tieEvent)){
+          gameTie()
+        }
+      }
   })
